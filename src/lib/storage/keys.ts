638ec87a54a293fd083c8d054dfc943fd59b3a72
selbarinssi <elbarinssis@gmail.com>
2026-09-22 14:00:09 +0100

    Create keys.ts

---
+++ src/lib/storage/keys.ts
@@ -0,0 +1,7 @@
+export const STORAGE_KEYS = {
+  neighborhoods: 'ap_neighborhoods',
+  clusters: 'ap_clusters',
+  vehicles: 'ap_vehicles',
+  orders: 'ap_orders',
+  dispatchConfig: 'ap_dispatch_config',
+} as const;
