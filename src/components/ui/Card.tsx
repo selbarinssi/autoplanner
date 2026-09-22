85216929407897ae92405380ea252e53db2b206e
selbarinssi <elbarinssis@gmail.com>
2026-09-22 16:36:37 +0100

    Update Card.tsx

--- src/components/ui/Card.tsx
+++ src/components/ui/Card.tsx
@@ -1,10 +1,17 @@
-interface CardProps {
+import { HTMLAttributes } from 'react';
+
+interface CardProps extends HTMLAttributes<HTMLDivElement> {
   children: React.ReactNode;
   className?: string;
   padding?: boolean;
 }
 
-export function Card({ children, className = '', padding = true }: CardProps) {
+export function Card({
+  children,
+  className = '',
+  padding = true,
+  ...props
+}: CardProps) {
   return (
     <div
       className={`
@@ -15,6 +22,7 @@ export function Card({ children, className = '', padding = true }: CardProps) {
         ${padding ? 'p-5' : ''}
         ${className}
       `}
+      {...props}
     >
       {children}
     </div>
